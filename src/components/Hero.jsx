73e3d6f2ca772c21b8a0f9d63115f94fbbe7fe3d
Vishal Sain <117062321+Vishallyours@users.noreply.github.com>
2025-04-10 11:39:40 +0000

    images upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,17 @@
+import HeroImage from "../assets/Crickter.jpg";
 function Hero() {
     return (
     <>
         <div className="md:flex
                         md:justify-center
                         md:items-center
-                        bg-black 
+                        bg-blue-800 
                         text-white
                         h-screen w-full
                      ">
-            
+                        
+            <img className="md:flex object-cover aspect-[16/9]
+            w-full h-screen" src={HeroImage} alt="Hero"/>
         </div>
     </>
     )
